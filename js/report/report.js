1f910950f405b0a1fc2dba5b95bc5c13680e868c
rocio <rociocpoppe@gmail.com>
2023-07-13 12:14:22 -0300

    feat: PlZ-5 add view for report

---
+++ js/report/report.js
@@ -0,0 +1,24 @@
+/**
+ * Fetch the report and append to the table
+ * 
+ * ****************************
+ * Please change 'json/report.json' 
+ * with your service endpoint below
+ * ****************************
+ */
+fetch('http://127.0.0.1:5000/report/')
+    .then(response => response.json())
+    .then(report => {
+        let template = createReportTemplate(report);
+        $("#report").append(template);
+    });
+
+/**
+ * Find the template tag and populate it with the data
+ * @param report 
+ */
+function createReportTemplate(report) {
+    let template = $("#report-item-template")[0].innerHTML;
+    return Mustache.render(template, report);
+}
+
